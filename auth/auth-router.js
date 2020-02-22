185d17be2c6831d91532c4c475df510af8bac39f
Chris Vasquez <chrvasq@outlook.com>
2020-02-21 18:32:33 -0800

    Add register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Users = require("./user-model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", async (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
+
+  try {
+    const saved = await Users.add(user);
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
